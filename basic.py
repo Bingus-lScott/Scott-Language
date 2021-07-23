ba09a3a7b51adffce85fe528b2c0ea6bdfb680d6
Landon Scott <baileygamer194@gmail.com>
2021-07-23 09:59:42 -0400

    Fixed shell errors and remove unknown shell module
    The shell error was the "<stdin>'' as it did not know about this for some reason even though it was just text

--- basic.py
+++ basic.py
@@ -10,17 +10,47 @@ DIGITS = '0123456789'
 
 class Error:
     # Creates an Error Template
-    def __init__(self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
     
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Charater', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Charater', details)
+
+#####################
+# POSITION
+#####################
+
+class Position:
+    def __init__(self, indx, ln, col, fn, ftxt):
+        self.indx = indx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.indx += 1
+        self.col += 1
+        
+        if current_char == "\n":
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.index, self.ln, self.col, self.fn, self.ftxt)
+
+
 
 #####################
 # TOKENS
@@ -52,15 +82,16 @@ class Token:
 #####################
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.indx] if self.pos.indx < len(self.text) else None
     
     def make_tokens(self):
         tokens = []
@@ -89,9 +120,10 @@ class Lexer:
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
-                self.advance
-                return [], IllegalCharError("'" + char + "'")
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
         return tokens, None
     
@@ -119,8 +151,8 @@ class Lexer:
 # RUN
 #####################
 
-def run(text):
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
 
     return tokens, error
