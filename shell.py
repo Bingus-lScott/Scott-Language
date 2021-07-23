af45703db2b62bd5e09ebee550b0c4a6f47eac31
Landon Scott <baileygamer194@gmail.com>
2021-07-23 10:25:13 -0400

    Changed shell to say N/A instead of Basic
    N/A stands for Not avaiable in this case which there is no file for it to read yet

--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 import basic
 
 while True:
-    text = input('basic > ')
+    text = input('N/A > ')
     result, error = basic.run('No file currently', text)
 
     if error: print(error.as_string())
